a4fc022b96e6de57ba5b17c71aa3ef6f7cec240c
Anik Adhikari <developwithanik@gmail.com>
2024-01-29 22:11:59 +0530

    Add tweet controller with TODOs

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+});
+
+export { createTweet, deleteTweet, getUserTweets, updateTweet };
