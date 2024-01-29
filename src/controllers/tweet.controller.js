99779061ada54c59473ade782e922c13fa9a37e7
Anik Adhikari <developwithanik@gmail.com>
2024-01-29 22:30:59 +0530

    Add createTweet functionality to tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,7 +1,32 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import Tweet from "../models/tweet.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
+// create tweet
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { tweetContent } = req.body;
+
+    if (!tweetContent) {
+        throw new ApiError(400, "Tweet content is required");
+    }
+
+    try {
+        const tweet = await Tweet.create({
+            owner: req.user?._id,
+            content: tweetContent,
+        });
+
+        if (!tweet) {
+            throw new ApiError(500, "Tweet not created");
+        }
+
+        res.status(201).json(
+            new ApiResponse(201, tweet, "Tweet created successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Unable to create tweet");
+    }
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
