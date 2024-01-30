c7935878fa504c715c747934fa584cb62a63899e
Anik Adhikari <developwithanik@gmail.com>
2024-01-30 17:40:31 +0530

    Add comment router

--- src/app.js
+++ src/app.js
@@ -21,11 +21,13 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // routes import
+import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import userRouter from "./routes/user.routes.js";
 
 // routes declaration
 app.use(API_V + "/users", userRouter);
 app.use(API_V + "/tweets", tweetRouter);
+app.use(API_V + "/comments", commentRouter);
 
 export default app;
