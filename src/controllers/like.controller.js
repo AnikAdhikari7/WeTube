73df4aab285f33e79bf093ff6303486337855e48
Anik Adhikari <developwithanik@gmail.com>
2024-02-08 22:47:13 +0530

    Add like controller and functions

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,26 @@
+// import { isValidObjectId } from "mongoose";
+// import Like from "../models/like.model.js";
+// import Video from "../models/video.model.js";
+// import ApiError from "../utils/ApiError.js";
+// import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    //TODO: toggle like on video
+});
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    //TODO: toggle like on comment
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    //TODO: toggle like on tweet
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+});
+
+export { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike };
