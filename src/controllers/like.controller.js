af661b789b4437d13aec35ba7c26d646d09f064f
Anik Adhikari <developwithanik@gmail.com>
2024-02-08 23:21:19 +0530

    Add toggleCommentLike to like.controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,4 +1,5 @@
 import { isValidObjectId } from "mongoose";
+import Comment from "../models/comment.model.js";
 import Like from "../models/like.model.js";
 import Video from "../models/video.model.js";
 import ApiError from "../utils/ApiError.js";
@@ -65,7 +66,61 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
-    //TODO: toggle like on comment
+
+    if (!commentId || !isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id");
+    }
+
+    const userId = req.user?._id;
+
+    try {
+        const comment = await Comment.findById(commentId);
+
+        if (!comment) {
+            throw new ApiError(404, "Comment not found");
+        }
+
+        const likeCriteria = { comment: commentId, likedBy: userId };
+
+        const alreadyLiked = await Like.findOne(likeCriteria);
+
+        if (alreadyLiked) {
+            // if already liked, then unlike
+            const deleted = await Like.findByIdAndDelete(alreadyLiked._id);
+
+            if (!deleted) {
+                throw new ApiError(500, "Failed to unlike comment");
+            }
+
+            return res
+                .status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        deleted,
+                        "Comment unliked successfully"
+                    )
+                );
+        } else {
+            // if not liked, then like
+            const newLike = await Like.create(likeCriteria);
+
+            if (!newLike) {
+                throw new ApiError(500, "Failed to like comment");
+            }
+
+            return res
+                .status(201)
+                .json(
+                    new ApiResponse(201, newLike, "Comment liked successfully")
+                );
+        }
+    } catch (error) {
+        throw new ApiError(
+            500,
+            error?.message || "Failed to toggle like on comment"
+        );
+    }
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
